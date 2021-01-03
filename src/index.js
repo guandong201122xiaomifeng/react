6675f734f0b6015f840509e93b2a98951c4f6c9c
xiaomifeng <2603501405@qq.com>
2021-01-03 11:14:50 +0800

    react first 2nd

--- src/index.js
+++ src/index.js
@@ -2,11 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AppContext from './AppContext'
 import reportWebVitals from './reportWebVitals';
+import Welcome from './Welcome'
+import WelcomeRender from './Welcome-render'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import NameCard from './components/NameCard'
+import LikesBtn from './components/LikesBtn'
+import DigitalClock from './components/DigitalClock'
+import CommentBox from './components/CommentBox'
+import CommentBoxFei from './components/CommentBoxFei'
 
+console.log('React', React)
+console.log('ReactDOM', ReactDOM)
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppContext />
+    {/* <App /> */}
+    {/* <Welcome /> */}
+    {/* <WelcomeRender /> */}
+    {/* <NameCard name="xiaoming" isHuman tags={['帅','体贴','善良']}/> */}
+    {/* <LikesBtn /> */}
+    {/* <DigitalClock a="1"/> */}
+    {/* <CommentBox /> */}
+    {/* <CommentBoxFei /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
